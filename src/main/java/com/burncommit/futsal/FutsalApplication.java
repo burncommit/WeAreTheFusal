639379a560ef1e963b9f52001a58ebf7ebb2a103
sana <burncommit@naver.com>
2022-12-14 22:27:06 +0900

    first commit

---
+++ src/main/java/com/burncommit/futsal/FutsalApplication.java
@@ -0,0 +1,13 @@
+package com.burncommit.futsal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutsalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FutsalApplication.class, args);
+	}
+
+}
